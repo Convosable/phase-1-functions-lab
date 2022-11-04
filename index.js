cd32d831803f7a3dafdfc758b85f4326e3ea9c02
Connor Vosberg <connorjv@gmail.com>
2022-11-04 05:56:27 -0700

    commit tree 3094674b7071205f47b4ef5fa460c2b66da39687

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocknumber) {
+    return Math.abs(blocknumber - 42)
+}
+distanceFromHqInBlocks(40)
+
+function distanceFromHqInFeet(blocknumber) {
+    return distanceFromHqInBlocks(blocknumber)*264
+}
+distanceFromHqInFeet(22)
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start - destination)*264)
+}
+distanceTravelledInFeet(30,44)
+
+function calculatesFarePrice(start, destination) {
+
+    let distance = distanceTravelledInFeet(start, destination)
+
+    if (distance <=400) {
+        return 0
+    } else if (distance >400 && distance<= 2000) {
+        return (distance - 400) *0.02 
+    } else if (distance >2000 && distance <=2500) {
+        return 25
+    } else if (distance >2500) {
+        return 'cannot travel that far'
+    }
+}
+
